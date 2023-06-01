89ddbf11c9d2a21b60de470e1dcf254ba265f327
leona-art <Leonagraph@gmail.com>
2023-06-01 23:27:25 +0900

    [feat]ファイルの読み取り

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -2,7 +2,15 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 fn main() {
-  tauri::Builder::default()
-    .run(tauri::generate_context!())
-    .expect("error while running tauri application");
+    tauri::Builder::default()
+        .invoke_handler(tauri::generate_handler![read_file])
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
+}
+
+/// ファイル内の文字列を返す
+#[tauri::command]
+fn read_file(path: String) -> Result<String, String> {
+    let contents = std::fs::read_to_string(path).map_err(|e| e.to_string())?;
+    Ok(contents)
 }
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,12 +1,17 @@
-import { component$, useSignal,$ } from '@builder.io/qwik';
+import { component$, useSignal,$ ,useVisibleTask$} from '@builder.io/qwik';
 import type { Signal } from '@builder.io/qwik';
 import {open as DialogOpen} from "@tauri-apps/api/dialog"
+import { invoke } from '@tauri-apps/api';
 
 export default component$(() => {
   const xml=useSignal("");
+  const xsl=useSignal("");
   return (
     <>
       <Dialog title="XML" ext='xml' content={xml} />
+      <pre>{xml}</pre>
+      <Dialog title="XSL" ext='xsl' content={xsl}/>
+      <pre>{xsl}</pre>
     </>
   );
 });
@@ -19,11 +24,17 @@ type DialogProps = {
 const Dialog = component$<DialogProps>(({title,content,ext}) => {
   const path=useSignal("")
   const select=$(async()=>{
-    const result = await DialogOpen({filters:[{name:title,extensions:[ext??"*"]}]}).catch(()=>"");
+    const result = await DialogOpen({filters:[{name:title,extensions:[ext??"*"]}]});
     if(result&&!Array.isArray(result)){
       path.value=result;
-      
     }
+  })
+  useVisibleTask$(async({track})=>{
+    track(()=>path.value)
+    if(content){
+      content.value= await invoke("read_file",{path:path.value}).catch(()=>"") as unknown as string;
+    }
+    
   })
   return (
     <>
@@ -34,12 +45,12 @@ const Dialog = component$<DialogProps>(({title,content,ext}) => {
   );
 })
 
-function xslt(xml:string,xslt:string){
-    const xsltProcessor = new XSLTProcessor();
-    const parser = new DOMParser();
-    const xmlDom = parser.parseFromString(xml, 'text/xml');
-    const xsltDom = parser.parseFromString(xslt, 'text/xml');
-    xsltProcessor.importStylesheet(xsltDom);
-    const resultDocument = xsltProcessor.transformToDocument(xmlDom);
-    return new XMLSerializer().serializeToString(resultDocument)
-}
+// function xslt(xml:string,xslt:string){
+//     const xsltProcessor = new XSLTProcessor();
+//     const parser = new DOMParser();
+//     const xmlDom = parser.parseFromString(xml, 'text/xml');
+//     const xsltDom = parser.parseFromString(xslt, 'text/xml');
+//     xsltProcessor.importStylesheet(xsltDom);
+//     const resultDocument = xsltProcessor.transformToDocument(xmlDom);
+//     return new XMLSerializer().serializeToString(resultDocument)
+// }
